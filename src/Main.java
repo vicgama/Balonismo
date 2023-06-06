5abb647f9a18f5d7adf7015b597ed1b17a7731bc
vicgama <victoriagama80@gmail.com>
2023-06-06 08:13:41 -0300

    Primeira classe

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String tom;
+        String zendaya;
+
+        tom = new String();
+        zendaya = new String();
+
+
+    }
+
+}
